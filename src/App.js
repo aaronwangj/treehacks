98f272e16130aaa964976178817a934c1bfbc6c8
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-02-18 01:27:43 -0800

    start working on landing pg

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import "./App.css";
 import Landing from "./screens/Landing";
 
 function App() {
-  return (
-    <div className="w-full h-full bg-red-400">
-      <Landing />
-    </div>
-  );
+  return <Landing />;
 }
 
 export default App;
--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -1,3 +1,14 @@
 export default function Landing() {
-  return <></>;
+  return (
+    <div className="w-screen h-screen bg-[url('landing.png')] bg-no-repeat bg-cover flex flex-col items-center justify-center space-y-10">
+      <h1 className="text-center font-extrabold text-6xl text-transparent bg-clip-text bg-gradient-to-r from-[#00df16] to-[#08bad2]">
+        Teach what you know. <br />
+        Effortlessly.
+      </h1>
+      <p className="text-center">
+        Transfer your knowledge to your audience with the best online course
+        platform for creators everywhere.
+      </p>
+    </div>
+  );
 }
