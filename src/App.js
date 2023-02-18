24d354b9f955cd91cca01d225ad0f41d4a015c98
jjaison <joeljaison13@berkeley.edu>
2023-02-18 02:33:01 -0800

    added dashboard page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Landing from "./screens/Landing";
+import Dashboard from "./screens/Dashboard";
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="w-full h-full bg-red-400">
+    <div class="width">
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
---
+++ src/screens/Dashboard.js
@@ -0,0 +1,51 @@
+import "./Dashboard.css";
+export default function Dashboard() {
+  let data = [
+    {
+      title: "How to Start a Youtube Channel",
+      price: 290,
+      description: "90+ Minutes of In-Depth Content On How I Built My Channel",
+    },
+    {
+      title: "How to Start a Youtube Channel",
+      price: 290,
+      description: "90+ Minutes of In-Depth Content On How I Built My Channel",
+    },
+    {
+      title: "How to Start a Youtube Channel",
+      price: 290,
+      description: "90+ Minutes of In-Depth Content On How I Built My Channel",
+    },
+  ];
+
+  return (
+    <div class="flex flex-col align-center" className="dashboard-wrapper">
+      <h1 className="welcome-txt">Welcome Aaron!</h1>
+      <div class="flex justify-between px-8">
+        <h2 className="course-msg">Your Courses</h2>
+        <h2 className="add-course">Add Course</h2>
+      </div>
+      <div className="divider"></div>
+      <div className="data-container">
+        {data.map((item) => {
+          return <Course item={item} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+const Course = ({ item }) => {
+  return (
+    <div className="course">
+      <div className="course-text">
+        <div className="data-title">{item.title}</div>
+        <h1>{item.description}</h1>
+      </div>
+      <div className="course-right">
+        <h1>{item.price}</h1>
+        <h1>picture</h1>
+      </div>
+    </div>
+  );
+};
