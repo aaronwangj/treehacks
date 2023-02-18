f7d44d814bb3cdf0c1c09e57df2fa7e0ddfe48c6
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-02-18 01:56:32 -0800

    fix landing page

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -5,10 +5,18 @@ export default function Landing() {
         Teach what you know. <br />
         Effortlessly.
       </h1>
-      <p className="text-center">
+      <p className="text-center text-xl w-1/2">
         Transfer your knowledge to your audience with the best online course
         platform for creators everywhere.
       </p>
+      <div className="w-1/2 h-14 flex space-x-16 justify-center items-center">
+        <button className="bg-[#7DC1FF] w-1/4 h-full rounded-full font-semibold text-xl">
+          Get Started
+        </button>
+        <button className="bg-[#9EFF9C] w-1/4 h-full rounded-full font-semibold text-xl">
+          Log In
+        </button>
+      </div>
     </div>
   );
 }
