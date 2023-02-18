203657e565f049f4f63eddfd281a8412fa106c5c
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-02-18 10:08:19 -0800

    add Publish page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Landing from "./screens/Landing";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Lessons from "./screens/Lessons";
 import LessonCreator from "./screens/LessonCreator";
+import Publish from "./screens/Publish";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/courses" element={<Courses />} />
         <Route path="/lessons" element={<Lessons />} />
         <Route path="/creator" element={<LessonCreator />} />
+        <Route path="/publish" element={<Publish />} />
       </Routes>
     </Router>
   );
