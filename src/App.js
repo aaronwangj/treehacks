9dd5fc34f3840e7ecbbf772ad52a90ea46d38de7
jjaison <joeljaison13@berkeley.edu>
2023-02-18 01:14:59 -0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Landing from "./screens/Landing";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="w-full h-full bg-red-400">
-      <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
     </div>
   );
 }
